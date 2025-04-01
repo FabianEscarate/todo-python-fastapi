94296503cacadf4d29ee7a545da4c6be45d1650c
Fabian Escarate <fescarate@outlook.com>
2025-03-31 23:25:34 -0300

    add uvcorn to app file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from typing import Union, Annotated
-from fastapi import FastAPI, HTTPException, Form, Query
-from models.Todo import Todo, Task
+from fastapi import FastAPI
 from routes.main import route
+import uvicorn
 
 class App():
   app = FastAPI()
 
   def start(self):
     self.app.include_router(route, prefix='/v1')
+    uvicorn.run(self.app, host="0.0.0.0")
     return self.app
